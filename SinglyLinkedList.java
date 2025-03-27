58b14da540a6999200060cdb2f3d8c1461aa63a6
Sanil <sanilguptagpt18@gmail.com>
2025-03-28 01:51:22 +0530

    Added more methods for LL

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -84,14 +84,66 @@ public class SinglyLinkedList {
         }
     }
 
+    public void deleteNodeFromBeginningOfLL(){
+        System.out.println("Deleted Node from Beginning of LL");
+        if(head == null){
+            System.out.println("Can't Delete Node as LL is Empty");
+            return;
+        }
+         if(head.next == null){
+             head = null;
+         }
+         else{
+             head = head.next;
+         }
+    }
+
+    public void deleteNodeFromEndOfLL(){
+        System.out.println("Deleted Node from Beginning of LL");
+        if(head.next == null){
+            head = null;
+        }else {
+            Node slow = head;
+            Node fast = slow.next;
+            while(fast.next != null){
+                slow = fast;
+                fast = fast.next;
+            }
+            slow.next = null;
+        }
+    }
+    public void deleteNodeFromSpecificPosition(int position){
+        if(head == null ){
+            System.out.println("Can't Delete Node as LL is empty!!");
+        }else if(head.next == null){
+            head = null;
+        }else {
+            Node slow = head;
+            Node fast = head.next;
+            for (int i = 1; i < position - 1; i++) {
+               slow = fast;
+               fast =  fast.next;
+            }
+            slow.next = fast.next;
+        }
+    }
+
 
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.insertNodeAtBeginningOfLL();
         singlyLinkedList.traversalOfLL();
+        System.out.println();
         singlyLinkedList.insertNodeAtEndOfLL();
         singlyLinkedList.traversalOfLL();
         singlyLinkedList.insertNodeAtSpecificPositionOfLL(2);
         singlyLinkedList.traversalOfLL();
+        singlyLinkedList.deleteNodeFromBeginningOfLL();
+        singlyLinkedList.traversalOfLL();
+        singlyLinkedList.deleteNodeFromEndOfLL();
+        singlyLinkedList.traversalOfLL();
+        singlyLinkedList.deleteNodeFromSpecificPosition(2);
+        singlyLinkedList.traversalOfLL();
+
     }
 }
